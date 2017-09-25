207cd09d7fc96507e91f433b382223b2dafe4d6d
Dominic Zhan <dominiczhan@gmail.com>
2017-09-26 07:29:44 +1000

    fixed indeding in half diamond

--- ch02-half-diamond.rb
+++ ch02-half-diamond.rb
@@ -1,11 +1,10 @@
 count = 0
 rows = 5
 rows.times do |row|
-    if row <= (rows - 1)/2
-    count = row + 1
-    
-    else
-        count = rows - row
-    end 
-        puts '*' * count 
+  if row <= (rows - 1)/2
+    count = row + 1  
+  else
+    count = rows - row
+  end 
+    puts '*' * count 
 end
